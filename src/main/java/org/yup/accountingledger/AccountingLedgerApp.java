118c82d2c944e8654ac438249a4e3931c064477b
Byoute@appdev.yearup.org <byoute@appdev.yearup.org>
2023-04-28 15:42:15 -0400

    changed the output of the main method

--- src/main/java/org/yup/accountingledger/AccountingLedgerApp.java
+++ src/main/java/org/yup/accountingledger/AccountingLedgerApp.java
@@ -4,7 +4,7 @@ public class AccountingLedgerApp {
 
     public static void main(String[] args){
 
-        System.out.println("hi");
+        System.out.println("this is the new text I wan to show");
 
     }
 }
